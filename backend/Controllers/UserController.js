2035b0e985a889ea274039027447ace5649e8a75
hisham1665 <hishamkhofficial@gmail.com>
2025-08-25 19:59:03 +0530

    fix the bug of build

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -1,5 +1,5 @@
 import User from '../models/UserModel.js';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 // Signup function
 export const signup = async (req, res) => {
